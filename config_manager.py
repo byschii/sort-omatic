a09449a61b784dfdb3b4a45010584df09fade8cd
Byschii <msbyschii@gmail.com>
2018-02-25 13:42:06 +0100

    added congig management

---
+++ config_manager.py
@@ -0,0 +1,32 @@
+import json
+from pprint import pprint
+
+class Config():
+	CONFIG_FILE_NAME = 'config.json'
+	data = {}
+
+	def __init__(self):
+		self.data = self.get_config()
+
+	def get_config(self):
+		return json.load(open(self.CONFIG_FILE_NAME))
+	def set_config(self):
+		with open(self.CONFIG_FILE_NAME, 'w+') as conf:
+			json.dump(self.data,conf)
+
+	def add(self,*args):
+		for k,v in args:
+			self.data[k]=v
+	def get(self,*args):
+		values = []
+		for v in args:
+			values.append(v)
+		return values
+	def is_set(self, *args):
+		return all(map( lambda x: x in self.data.keys(),args ))
+
+	def empty(self):
+		self.data = {}
+
+	def __str__(self):
+		return self.data.__str__()
--- sort-omatic.py
+++ sort-omatic.py
@@ -1,6 +1,16 @@
+from __future__ import print_function
 import os
+import os.path
 import platform as pl
 from getpass import getuser as user_name
+from config_manager import Config
+
+
+
+
+
+
+
 
 def get_os():
 	name = pl.system() #era figo anche .uname
@@ -30,13 +40,37 @@ def check_download_path(dl_path):
 		return False
 
 def main():
-	download_path, _ = get_downloads_and_documents_path()
-	if check_download_path(download_path):
-		print('directory found')
-		print(os.getcwd())
+	conf = Config()
+	if not conf.is_set("downloads_path", "documents_path"):
+		downloads_path, documents_path = get_downloads_and_documents_path()
+		conf.add(("downloads_path",downloads_path),("documents_path",documents_path))
+	else:
+		downloads_path, documents_path = conf.get("downloads_path", "documents_path")
+
+	print(downloads_path, documents_path)
+	'''
+	if check_download_path(downloads_path):
+		root, dirs, files = os.walk('.').next()
+		print(root, dirs, files)
+			
+
 	else:
 		raise Exception("Download directory not found")
-	
+	'''
+
+
+
+
+any
 
 if __name__ == '__main__':
 	main()
+
+
+'''
+
+os.path.split(path)
+
+    Split the pathname path into a pair, (head, tail) where tail is the last pathname component and head is everything leading up to that. The tail part will never contain a slash; if path ends in a slash, tail will be empty. If there is no slash in path, head will be empty. If path is empty, both head and tail are empty. Trailing slashes are stripped from head unless it is the root (one or more slashes only). In all cases, join(head, tail) returns a path to the same location as path (but the strings may differ). Also see the functions dirname() and basename().
+
+'''
